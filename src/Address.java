a4d81e7b5031fd930170c943a5c2cbb970b31974
AdamProgramista <adam.programista@poczta.fm>
2018-02-03 11:22:01 +0100

    saturday 2.0

--- src/Address.java
+++ src/Address.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Address {
+    int floor;
+    int stand;
+
+    public Address(int floor, int stand){
+        this.floor = floor;
+        this.stand = stand;
+    }
 }
